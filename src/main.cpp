725c06febb06126e981877cc39ffb18e22ac2fcf
Simran Kucheria <simran@mac.mynetworksettings.com>
2024-11-27 16:37:05 -0500

    Initial main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "mbed.h"
+
+
+int main(){
+  // Setup Code: Code meant to run only once
+
+  while(1){
+    // Loop Code: Code meant to run indefinitely (Infinite Loop!)
+    thread_sleep_for(1000); // Sleep (Embedded Delay)
+  }
+}
